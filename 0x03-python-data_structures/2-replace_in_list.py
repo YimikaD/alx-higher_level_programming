2c9fa5784eca2af7f641fc8360f107055af9d8f9
YimikaD <babsyinny@gmail.com>
2023-08-12 08:32:56 -0700

    Replace elementand chmod

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    if (idx < 0):
+        return (my_list)
+
+    if (my_list[idx: idx+1] == []):
+        return(my_list)
+
+    my_list[idx] = element
+    return(my_list)

604e5a07cb7355f44fb789905f6321a88dc7bcc6
YimikaD <babsyinny@gmail.com>
2023-09-12 15:29:20 -0700

    New files

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name: First name of the student.
+            last_name: Last name of the student.
+            age: Age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Get a dictionary representation of student."""
+        return self.__dict__

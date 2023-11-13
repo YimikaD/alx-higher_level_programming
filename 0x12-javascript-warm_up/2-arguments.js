84472088ada524d7761a72252193da0ffa5bdb74
YimikaD <babsyinny@gmail.com>
2023-11-13 11:12:23 -0800

    Task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-
+//script that prints a message depending of the number of arguments passed
 let myArgs = process.argv.slice(2);
 let argCount = myArgs.length;
 
-if (myArgs == 0) {
+if (argCount == 0) {
 	console.log('No argument');
-} else if (myArgs == 1) {
+} else if (argCount == 1) {
 	console.log('Argument found');
 } else {
 	console.log('Argument found');

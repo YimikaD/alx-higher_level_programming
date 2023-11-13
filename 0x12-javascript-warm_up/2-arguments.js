337dd9b6ca5dcd06d14d9c036fee6bffad18059b
YimikaD <babsyinny@gmail.com>
2023-11-13 11:24:57 -0800

    Task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -8,9 +8,9 @@ const argCount = myArgs.length;
  * checks number of arguments
  */
 if (argCount == 0) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (argCount == 1) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }

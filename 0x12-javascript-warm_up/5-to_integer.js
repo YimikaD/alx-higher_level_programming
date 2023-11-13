bc1adda0cea986b80dae6bef94b506ba84587282
YimikaD <babsyinny@gmail.com>
2023-11-13 15:19:40 -0800

    Task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const myargs = process.argv;
 const intnum = parseInt(myargs[2]);
-if (!isNaN(intnum)) {
-	console.log('My number: $(intnum)');
+if (isNaN(intnum)) {
+	console.log('Not a number');
 } else {
-	console.log('Not anumber');
+	console.log('My number: $(intnum[2])');
 }
